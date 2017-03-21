277c0672c0bc1ddfd6de6e1f084f9e80e3a42102
Daniel Gront <grount1@gmail.com>
2017-03-21 10:56:08 +0200

    All functionality and multiline textbox working.

--- DirectoryUtility/DirectoryUtility/Form1.Designer.cs
+++ DirectoryUtility/DirectoryUtility/Form1.Designer.cs
@@ -28,12 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.directoryBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.directoryTextBox = new System.Windows.Forms.TextBox();
+            this.organizeFilesStartButton = new System.Windows.Forms.Button();
+            this.organizeFilesLabel = new System.Windows.Forms.Label();
+            this.progressTextBox = new System.Windows.Forms.TextBox();
+            this.fileProgressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(13, 11);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(98, 23);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse directory";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // directoryTextBox
+            // 
+            this.directoryTextBox.Location = new System.Drawing.Point(118, 12);
+            this.directoryTextBox.Name = "directoryTextBox";
+            this.directoryTextBox.Size = new System.Drawing.Size(672, 20);
+            this.directoryTextBox.TabIndex = 1;
+            // 
+            // organizeFilesStartButton
+            // 
+            this.organizeFilesStartButton.Location = new System.Drawing.Point(13, 64);
+            this.organizeFilesStartButton.Name = "organizeFilesStartButton";
+            this.organizeFilesStartButton.Size = new System.Drawing.Size(98, 23);
+            this.organizeFilesStartButton.TabIndex = 2;
+            this.organizeFilesStartButton.Text = "Start";
+            this.organizeFilesStartButton.UseVisualStyleBackColor = true;
+            this.organizeFilesStartButton.Click += new System.EventHandler(this.organizeFilesStartButton_Click);
+            // 
+            // organizeFilesLabel
+            // 
+            this.organizeFilesLabel.AutoSize = true;
+            this.organizeFilesLabel.Location = new System.Drawing.Point(23, 48);
+            this.organizeFilesLabel.Name = "organizeFilesLabel";
+            this.organizeFilesLabel.Size = new System.Drawing.Size(73, 13);
+            this.organizeFilesLabel.TabIndex = 3;
+            this.organizeFilesLabel.Text = "Organize Files";
+            // 
+            // progressTextBox
+            // 
+            this.progressTextBox.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.progressTextBox.Location = new System.Drawing.Point(13, 93);
+            this.progressTextBox.Multiline = true;
+            this.progressTextBox.Name = "progressTextBox";
+            this.progressTextBox.ReadOnly = true;
+            this.progressTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.progressTextBox.Size = new System.Drawing.Size(777, 223);
+            this.progressTextBox.TabIndex = 4;
+            // 
+            // fileProgressBar
+            // 
+            this.fileProgressBar.Location = new System.Drawing.Point(12, 322);
+            this.fileProgressBar.Name = "fileProgressBar";
+            this.fileProgressBar.Size = new System.Drawing.Size(777, 23);
+            this.fileProgressBar.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(802, 357);
+            this.Controls.Add(this.fileProgressBar);
+            this.Controls.Add(this.progressTextBox);
+            this.Controls.Add(this.organizeFilesLabel);
+            this.Controls.Add(this.organizeFilesStartButton);
+            this.Controls.Add(this.directoryTextBox);
+            this.Controls.Add(this.browseButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Directory Utility";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog directoryBrowserDialog;
+        private System.Windows.Forms.Button browseButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox directoryTextBox;
+        private System.Windows.Forms.Button organizeFilesStartButton;
+        private System.Windows.Forms.Label organizeFilesLabel;
+        private System.Windows.Forms.TextBox progressTextBox;
+        private System.Windows.Forms.ProgressBar fileProgressBar;
     }
 }
 
--- DirectoryUtility/DirectoryUtility/Form1.cs
+++ DirectoryUtility/DirectoryUtility/Form1.cs
@@ -7,14 +7,103 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
 
 namespace DirectoryUtility
 {
     public partial class Form1 : Form
     {
+        string selectedPath = "";
+        bool isPathSelected = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            browseFilesDialog();
+        }
+
+        private void browseFilesDialog()
+        {
+            progressTextBox.Clear();
+
+            if (directoryBrowserDialog.ShowDialog() == DialogResult.OK)
+            { 
+                selectedPath = directoryBrowserDialog.SelectedPath;
+                directoryTextBox.Text = selectedPath;
+                isPathSelected = true;
+            }
+
+        }
+
+        private void organizeFilesStartButton_Click(object sender, EventArgs e)
+        {
+            if (isPathSelected == true) 
+            {
+                if (Directory.Exists(selectedPath)) // might be useles??
+                {
+                    string[] files = Directory.GetFiles(selectedPath, "*.*", SearchOption.TopDirectoryOnly);
+
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        string extenstion = Path.GetExtension(files[i]);
+                        string extenstionWithoutDot = "";
+
+                        if (extenstion != "")
+                        {
+                            extenstionWithoutDot = extenstion.Substring(1, extenstion.Length - 1);
+                        }
+                        else
+                        {
+                            extenstionWithoutDot = "File";
+                        }
+
+                        string newPath = selectedPath;
+                        string fileName = Path.GetFileName(files[i]);
+
+                        newPath += "\\" + extenstionWithoutDot;
+                        DirectoryInfo dirInfo = Directory.CreateDirectory(newPath);
+                        newPath += "\\" + fileName;
+                        System.IO.File.Move(files[i], newPath);
+
+                        printActionsToTextBox(fileName, dirInfo.FullName, eAction.Move);
+                    
+                    }
+
+                    if (MessageBox.Show("Do you want to enter the directory?", "Opeartiong finished", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) 
+                        == DialogResult.OK)
+                    {
+                        Process.Start("explorer.exe", selectedPath);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a folder", "No folder selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                browseFilesDialog();
+            }
+        }
+        void printActionsToTextBox(string fileName, string newDirectory, eAction actionType)
+        {
+            if (actionType == eAction.Move)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine(String.Format("Moving {0} to {1}", fileName, newDirectory));
+                progressTextBox.Text += sb.ToString();
+                progressTextBox.Refresh();
+                progressTextBox.SelectionStart = progressTextBox.Text.Length;
+                progressTextBox.ScrollToCaret();
+            }
+        }
+
+        private enum eAction
+        {
+            Move
         }
     }
 }
