7955ffbce4460a0f624ac3bf237da3ed69c4fa79
Daniel Gront <grount1@gmail.com>
2017-07-08 17:50:11 +0300

    Removed Parallel loop untill further clarifications, and minor improvements.

--- DirectoryUtility/DirectoryUtility/MainWindow.cs
+++ DirectoryUtility/DirectoryUtility/MainWindow.cs
@@ -1,22 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Diagnostics;
-using System.Threading;
 
 namespace DirectoryUtility
 {
     public partial class MainWindow : Form
     {
-        string selectedPath = "";
-        bool isPathSelected;
+        private string selectedPath = "";
+        private bool isPathSelected;
 
         public MainWindow()
         {
@@ -34,56 +27,48 @@ namespace DirectoryUtility
         private void browseFilesDialog()
         {
             progressTextBox.Clear();
+            fileProgressBar.Minimum = 1;
             fileProgressBar.Value = 1;
 
             if (directoryBrowserDialog.ShowDialog() == DialogResult.OK)
-            { 
+            {
                 selectedPath = directoryBrowserDialog.SelectedPath;
                 directoryTextBox.Text = selectedPath;
                 isPathSelected = true;
             }
-
         }
 
         private void handleFileExtension(ref string extensionWithoutDot, ref string extension)
         {
-            if (extension != "")
-            {
-                extensionWithoutDot = extension.Substring(1, extension.Length - 1);
-            }
-            else
-            {
-                extensionWithoutDot = "File";
-            }
+            if (extensionWithoutDot == null) throw new ArgumentNullException(nameof(extensionWithoutDot));
+            extensionWithoutDot = extension != "" ? extension.Substring(1, extension.Length - 1) : "File";
         }
 
-        static object organizeLock = new object();
         private void organizeFilesStartButton_Click(object sender, EventArgs e)
         {
-            if (isPathSelected == true) 
+            if (isPathSelected)
             {
                 progressTextBox.Clear();
 
-                if (Directory.Exists(selectedPath)) 
+                if (Directory.Exists(selectedPath))
                 {
                     string[] files = Directory.GetFiles(selectedPath, "*.*", SearchOption.TopDirectoryOnly);
-                    fileProgressBar.Maximum = files.Length;
-                    string saveFilePath;
                     bool fileUsedState = true;
-                    
 
-                    Parallel.For(0, files.Length, i =>
+                    fileProgressBar.Maximum = files.Length;
+
+                    foreach (string t in files)
                     {
-                        string extension = Path.GetExtension(files[i]);
+                        string extension = Path.GetExtension(t);
                         string extenstionWithoutDot = "";
 
                         handleFileExtension(ref extenstionWithoutDot, ref extension);
 
                         string newPath = selectedPath;
-                        string fileName = Path.GetFileName(files[i]);
+                        string fileName = Path.GetFileName(t);
 
                         newPath += "\\" + extenstionWithoutDot;
-                        saveFilePath = newPath;
+                        var saveFilePath = newPath;
                         DirectoryInfo dirInfo = Directory.CreateDirectory(newPath);
                         newPath += "\\" + fileName;
 
@@ -91,30 +76,30 @@ namespace DirectoryUtility
 
                         try
                         {
-                            System.IO.File.Move(files[i], newPath);
+                            File.Move(t, newPath);
                         }
-                        catch (System.IO.IOException)
+                        catch (IOException)
                         {
                             fileUsedState = false;
                         }
 
-                        lock(organizeLock)
-                        {
-                            fileProgressBar.PerformStep();
+                        fileProgressBar.PerformStep();
 
-                            if (fileUsedState == true)
-                            {
-                                printActionsToTextBox(fileName, dirInfo.FullName, eAction.Move);
-                            }
+                        if (fileUsedState)
+                        {
+                            printActionsToTextBox(fileName, dirInfo.FullName, eAction.Move);
                         }
-                    });
-                    
+                    }
+
+
                     if (fileUsedState == false && files.Length == 1)
                     {
-                        MessageBox.Show("Cannot move file, Currently in use", "Operation Failed",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show("Cannot move file, Currently in use", "Operation Failed", MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
                     }
-                    else if (MessageBox.Show("Do you want to enter the directory?", "Opeartiong finished", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) 
-                        == DialogResult.OK)
+                    else if (MessageBox.Show("Do you want to enter the directory?", "Opeartiong finished",
+                                 MessageBoxButtons.OKCancel, MessageBoxIcon.Question)
+                             == DialogResult.OK)
                     {
                         Process.Start("explorer.exe", selectedPath);
                     }
@@ -126,12 +111,13 @@ namespace DirectoryUtility
             }
         }
 
+
         private void ifDuplicateAddDate(ref string path, string pathWithFile)
         {
-            if(System.IO.File.Exists(path) == true)
+            if (System.IO.File.Exists(path) == true)
             {
                 DateTime thisDay = DateTime.Now;
-                string thisDayString = String.Format("{0:dd-MM-HH_HH-mm-ss}", thisDay);
+                string thisDayString = $"{thisDay:dd-MM-HH_HH-mm-ss}";
                 string extension = Path.GetExtension(path);
                 string onlyFileName = Path.GetFileNameWithoutExtension(path);
                 onlyFileName += "_" + thisDayString;
@@ -140,12 +126,13 @@ namespace DirectoryUtility
                 path = pathWithFile;
             }
         }
+
         void printActionsToTextBox(string fileName, string newDirectory, eAction actionType)
         {
             if (actionType == eAction.Move)
             {
                 StringBuilder sb = new StringBuilder();
-                sb.AppendLine(String.Format("Moving {0} to {1}", fileName, newDirectory));
+                sb.AppendLine($"Moving {fileName} to {newDirectory}");
                 progressTextBox.Text += sb.ToString();
                 progressTextBox.Refresh();
                 progressTextBox.SelectionStart = progressTextBox.Text.Length;
@@ -155,7 +142,8 @@ namespace DirectoryUtility
 
         private void invalidPath()
         {
-            MessageBox.Show("Please select a folder", "No folder selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("Please select a folder", "No folder selected", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
             browseFilesDialog();
         }
 
